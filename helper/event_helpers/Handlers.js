a1d1d56b5af7e2961873b55be8ae44180a0ed57f
bibekkakati <bibekkakati0@gmail.com>
2020-09-25 22:45:33 +0530

    Updated

--- helper/event_helpers/Handlers.js
+++ helper/event_helpers/Handlers.js
@@ -1,6 +1,7 @@
 const Singleton_queue = require("../queue_helpers/Singleton");
 const Singleton_worker = require("../../worker/Singleton");
 const EnqueueTask = require("../task_helpers/EnqueueTask");
+const EmitEvent = require("./Emitters");
 
 const invokeWorker = () => {
 	const taskQueue = Singleton_queue.getQueue();
@@ -16,6 +17,7 @@ const invokeWorker = () => {
 		} else if (result) {
 			//TODO: mark the task success in database
 		}
+		EmitEvent("invoke_worker");
 	});
 };
 
